792e7c1c55b0fc20d2d6f679263e57daa64837e4
Mark Friesen <mpfriesen+git@gmail.com>
2021-03-08 02:29:32 -0800

    Deleting unneeded files

--- inTurf.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    interpolate: require('@turf/interpolate'),
-    centroid: require('@turf/centroid'),
-    collect: require('@turf/collect'),
-    helpers: require('@turf/helpers')
-
-};
